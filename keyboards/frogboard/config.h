338cb230003102cf01e2fa8fa45f4814b4afb72e
IllusionaryFrog <git@illusionaryfrog.com>
2024-10-09 14:45:40 +0000

    initial frogboard c files

---
+++ keyboards/frogboard/config.h
@@ -0,0 +1,7 @@
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 150U
+
+#define SERIAL_USART_FULL_DUPLEX
+// #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
