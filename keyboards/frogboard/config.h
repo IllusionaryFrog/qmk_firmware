5e3fb5cd7ca163ab915e6deabe39c535349246cc
Illusionary Frog <git@illusionaryfrog.com>
2025-01-11 13:17:40 +0100

    updated files

--- keyboards/frogboard/config.h
+++ keyboards/frogboard/config.h
@@ -3,5 +3,6 @@
 
 #define SERIAL_USART_FULL_DUPLEX
 // #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+// #define SERIAL_USART_PIN_SWAP // Swap TX and RX pins if keyboard is master halve. (Only available on some MCUs)
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
